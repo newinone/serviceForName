0cf390b7be803466a60dcdac03cc47dc487c4f8d
Ingrid Niu <ingrid.niu@pason.com>
2018-01-20 20:50:51 -0700

    Fix for issue #1124: corrected ServiceTypeTest

--- core-test/src/main/java/org/openstack4j/openstack/common/ServiceTypeTest.java
+++ core-test/src/main/java/org/openstack4j/openstack/common/ServiceTypeTest.java
@@ -1,71 +1,65 @@
-package org.openstack4j.api.types;
+package org.openstack4j.openstack.common;
 
-import java.util.regex.Pattern;
-public enum ServiceType {
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+import org.openstack4j.api.types.ServiceType;
+import static org.testng.Assert.assertEquals;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Test;
 
-	IDENTITY("keystone", "identity"),
-	APP_CATALOG("murano", "application-catalog"),
-	COMPUTE("nova", "compute"),
-	IMAGE("glance", "image"),
-	BLOCK_STORAGE("cinder", "volume"),
-	OBJECT_STORAGE("object-store", "object-store"),
-	NETWORK("neutron", "network"),
-	OCTAVIA("octavia", "load-balancer"),
-	EC2("ec2", "ec2"),
-	TELEMETRY("ceilometer", "metering"),
-	TELEMETRY_AODH("aodh", "alarming"),
-	ORCHESTRATION("heat", "orchestration"),
-	CLUSTERING("senlin", "clustering"),
-	SAHARA("sahara", "data_processing"),
-	SHARE("manila", "share"),
-	DATABASE("trove","database"),
-	BARBICAN("barbican", "key-manager"),
-	TACKER("tacker", "nfv-orchestration"),
-	ARTIFACT("glare", "artifact"),
-  	MAGNUM("magnum", "container"),
-	DNS("designate", "dns"),
-	WORKFLOW("mistral", "workflow"),
-	UNKNOWN("NA", "NA")
-	;
+public class ServiceTypeTest {
 
-	private final String serviceName;
-	private final String type;
-    private final Pattern servicePattern;
-    private static final String SERVICE_PATTERN_SUFFIX = "[v|\\d|\\.]*";
+   private Map<ServiceType, Collection<String>> types;
 
-	ServiceType(String serviceName, String type) {
-		this.serviceName = serviceName;
-		this.type = type;
-        this.servicePattern = Pattern.compile(Pattern.quote(serviceName) + SERVICE_PATTERN_SUFFIX +
-                        "|" + Pattern.quote(type) + SERVICE_PATTERN_SUFFIX +
-                        "|" + Pattern.quote(this.name()) + SERVICE_PATTERN_SUFFIX
-                , Pattern.CASE_INSENSITIVE);
-    }
+    private Map<ServiceType, Collection<String>> unknownTypes;
 
-	public String getServiceName() {
-		return this.serviceName;
-	}
+   @BeforeSuite
+   public void setup() {
+       types = new HashMap<>();
+       types.put(ServiceType.IDENTITY, Arrays.asList("identity","identityV2","identityv3"));
+       types.put(ServiceType.APP_CATALOG, Arrays.asList("application-catalog", "Application-catalogv2", "application-Catalogv3"));
+       types.put(ServiceType.COMPUTE, Arrays.asList("compute", "COMPUTEv2", "computeV3"));
+       types.put(ServiceType.IMAGE, Arrays.asList("image", "imageV2", "imagev3"));
+       types.put(ServiceType.BLOCK_STORAGE, Arrays.asList("volume", "volumev1", "volumev2"));
+       types.put(ServiceType.OBJECT_STORAGE, Arrays.asList("obJect-stOre", "object-storev2", "object-storev3"));
+       types.put(ServiceType.NETWORK, Arrays.asList("network", "networkv2.0", "networkV3"));
+       types.put(ServiceType.EC2, Arrays.asList("EC2","ec2V2","ec2v3"));
+       types.put(ServiceType.TELEMETRY, Arrays.asList("metering","meteringV2.0","meteringv3"));
+       types.put(ServiceType.TELEMETRY_AODH, Arrays.asList("alarming","alarmingV2","alarmingV3"));
+       types.put(ServiceType.ORCHESTRATION, Arrays.asList("orchestration","orchestrationv2","orchestrationv3"));
+       types.put(ServiceType.CLUSTERING, Arrays.asList("clustering","clusteringV2","clustering3"));
+       types.put(ServiceType.SAHARA, Arrays.asList("data_processing","data_processingV1","data_processingv3"));
+       types.put(ServiceType.SHARE, Arrays.asList("share","sharev2","shareV3"));
+       types.put(ServiceType.DATABASE, Arrays.asList("database","databaseV2","Databasev3"));
+       types.put(ServiceType.BARBICAN, Arrays.asList("key-manager","key-managerv2","key-managerv3"));
+       types.put(ServiceType.TACKER, Arrays.asList("nfv-orchestration","nfv-orchestrationv2","nfv-orchestration3"));
+       types.put(ServiceType.ARTIFACT, Arrays.asList("artifact","artifactv2","artifactv3"));
+       types.put(ServiceType.MAGNUM, Arrays.asList("container","ContainerV3","containerv1"));
+       types.put(ServiceType.DNS, Arrays.asList("dns","dnsv2","dnsV3"));
+       types.put(ServiceType.WORKFLOW, Arrays.asList("workflow","workflowv3","workflowv2"));
 
-	public String getType() {
-		return this.type;
-	}
+       unknownTypes = new HashMap();
+       unknownTypes.put(ServiceType.ORCHESTRATION, Arrays.asList("heat-cfg","heatother","heatvm","heat-cfg4"));
 
-    private Pattern getServicePattern()
-    {
-        return this.servicePattern;
-    }
+   }
 
-    public static ServiceType forName(String name)
-    {
-        if (name == null || name.isEmpty())
-        {
-            return ServiceType.UNKNOWN;
+   @Test
+   public void testNameResolveByType() {
+        for (Map.Entry<ServiceType, Collection<String>> entry : types.entrySet()) {
+           for(String type : entry.getValue()){
+               assertEquals(entry.getKey(), ServiceType.forName(type));
+           }
+       }
+   }
+
+    @Test
+    public void testNameNotResolved() {
+        for (Map.Entry<ServiceType, Collection<String>> entry : unknownTypes.entrySet()) {
+            for(String type : entry.getValue()){
+                assertEquals(ServiceType.UNKNOWN, ServiceType.forName(type));
+            }
         }
-        for (ServiceType s : ServiceType.values())
-        {
-            if(s.getServicePattern().matcher(name).matches())
-                return s;
-        }
-        return ServiceType.UNKNOWN;
     }
 }
